00f288059454b04fa099e12516d90e500ee48d08
yudax42 <yudax42@gmail.com>
2023-10-14 13:05:22 +0100

    chore: imporve build

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,28 +1,65 @@
 import { build } from 'esbuild'
 import { Generator } from 'npm-dts'
+import fs from 'node:fs';
+import path from 'node:path';
+import { mkdir , rm, } from "node:fs/promises";
 
-new Generator({
-  entry: 'src/index.ts',
-  out: 'dist/index.d.ts',
-}).generate()
+const stdout = console
 
-const shared = {
-  entryPoints: ['src/index.ts'],
-  bundle: true,
-  minify: true,
+const targets: Parameters<typeof build>[0][] = [
+  {
+    platform: "node",
+    outfile: "dist/index.js",
+  },
+  {
+    platform: "neutral",
+    format: "esm",
+    outfile: "dist/index.esm.js",
+  },
+];
+
+async function prepare() {
+    const dist = path.join(process.cwd(), 'dist')
+
+    if(fs.existsSync(dist)) {
+        await rm(dist, { recursive: true })
+    }
+
+    await mkdir(dist)
 }
 
-// cjs
-build({
-  ...shared,
-  platform: 'node',
-  outfile: 'dist/index.js',
-})
+async function generateTypes() {
+    const generator = new Generator({
+        entry: 'src/index.ts',
+        output: 'dist/index.d.ts'
+    })
 
-// esm
-build({
-  ...shared,
-  platform: 'neutral',
-  format: 'esm',
-  outfile: 'dist/index.esm.js',
-})
+    await generator.generate()
+}
+
+async function buildTargets() {
+    for (const target of targets) {
+        await build({
+            entryPoints: ['src/index.ts'],
+            bundle: true,
+            minify: true,
+            treeShaking: true, 
+            ...target,
+        })
+    }
+}
+
+async function main() {
+    stdout.log('Building...')
+    await prepare()
+    
+    stdout.log('Building targets...')
+    await buildTargets();
+
+    stdout.log('Generating types...')
+    await generateTypes()
+
+    stdout.log('Done!')
+}
+
+main()
